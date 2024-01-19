4816293b6cb2bd02afb0e1aadab1422c0d33840e
adhamo0o <adhamelmesady414@gmail.com>
2024-01-19 10:45:52 -0800

    4

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-if i not in(101, 113):
-print("{letter:c}".format(letter=i), end="")
+    if i not in (101, 113):
+        print("{letter:c}".format(letter=i), end="")
