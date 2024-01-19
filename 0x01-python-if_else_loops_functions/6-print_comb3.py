0fb839f4b9edad8e1d7bb12201d8ce70c4a7836a
adhamo0o <adhamelmesady414@gmail.com>
2024-01-19 14:32:10 -0800

    7

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for i in range(10):
     for x in range(i + 1, 10):
-        print("{:d}{:d}, " .format(i, x), end="")
-print("")
+        if (i, x) != (8, 9):
+            print("{:d}{:d}, " .format(i, x), end="")
+print("89")
