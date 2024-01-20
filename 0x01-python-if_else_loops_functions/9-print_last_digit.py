bc75bd7363491e6712ed26b4700db11ef790d1ab
adhamo0o <adhamelmesady414@gmail.com>
2024-01-20 04:40:49 -0800

    10

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number in range (0, 10):
-        print(f"{number[-1]}")
+    for x in numbers:
+        if x in range (-9, 10):
+            print(f"{x[-1]}")
