6fceba8843d1b3fad7932f4c56becb3bc1b2e0d8
adhamo0o <adhamelmesady414@gmail.com>
2024-01-20 03:56:25 -0800

    9

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    if ord(str) >= 97 and ord(str) <= 122:
+        new_str = str - 32
+        print("{new_str:c}".format(new_str))
+    else:
+        print("{str:c}".format(str))
