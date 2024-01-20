f27abaf782be9825eee1bbe9c8948546409a6cdd
adhamo0o <adhamelmesady414@gmail.com>
2024-01-20 03:31:58 -0800

    8

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if c >= ord('a') and ord(c) <=  ord(('z') + 1)):
+    if c >= ord('a') and c <= ord(('z') + 1)):
         return true
     else:
         return false
