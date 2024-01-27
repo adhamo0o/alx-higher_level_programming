4c0261cf27762b1365c8210904943409184c2370
adhamo0o <adhamelmesady414@gmail.com>
2024-01-26 16:46:20 -0800

    6

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in range(65, 91) and i in range(97, 123):
-        if i not in(67, 99):
-            return my_string
+    new = my_string.translate({ord(x): None for x in 'cC'})
+    return new
