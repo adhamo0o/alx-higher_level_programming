e006fcb361072bfa5cc00070fa954e82f7f06e52
adhamo0o <adhamelmesady414@gmail.com>
2024-01-27 09:52:04 -0800

    8

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,5 +4,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a = tuple_a + (0,)
     if len(tuple_b < 2):
         tuple_b = tuple_b + (0,)
-    result = tuble_a + tuple_b
+    result = tuble_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
     return result
