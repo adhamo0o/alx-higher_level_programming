981823d84b76de7ef0aab10e6c48993e8eaf5048
adhamo0o <adhamelmesady414@gmail.com>
2024-01-29 16:15:44 -0800

    2

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = []
+    for i in range(len(my_list)):
+        if i == search:
+            i == replace
+            new_list = my_list[:]
+    return new_list
