6d3df25aec3c3ae4670d77782648f01d050dd4e8
adhamo0o <adhamelmesady414@gmail.com>
2024-02-05 15:35:09 -0800

    1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        for x in my_list:
-            print(x, end=" ")
-            x += 1
+        for element in my_list:
+            print(element, end=" ")
+            element += 1
     except:
         pass
     return x
