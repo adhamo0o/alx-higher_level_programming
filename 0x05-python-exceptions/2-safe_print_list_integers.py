a1a572a7d07212e7b9dcb7096eac7cab6784362c
adhamo0o <adhamelmesady414@gmail.com>
2024-02-05 17:45:09 -0800

    3

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,11 +3,9 @@ def safe_print_list_integers(my_list=[], x=0):
     first = 0
     try:
         for i in range(x):
-            while first == x:
-                print("{:d}".format(mu_list[i]), end="")
-                first += 1
-    except Exception:
-        pass
-    finally:
-        print()
+            print("{:d}".format(my_list[i]), end="")
+            first += 1
+    except (TypeError, ValueError):
+        continue
+    print()
     return first
