92a7f1ffa40f3bc3fd71fb759820a6bde6d68e7a
adhamo0o <adhamelmesady414@gmail.com>
2024-02-05 16:04:07 -0800

    2

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except TypeError:
+        print("{} is not an integer".format(value))
+        return False
+    except Exception:
+        pass
+    finally:
+        print()
