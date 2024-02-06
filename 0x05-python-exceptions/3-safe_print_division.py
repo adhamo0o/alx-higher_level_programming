67afc8588c22707c6ce8efa8ab1825082d7fafb7
adhamo0o <adhamelmesady414@gmail.com>
2024-02-05 18:03:46 -0800

    4

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,5 +6,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         return None
     finally:
-        print("Inside result: {:d}".format(div))
+        print("Inside result: {}".format(div))
         return div
