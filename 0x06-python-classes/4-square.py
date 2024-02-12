47f143fbb446468c2c0e89d0e528890e8d8339e0
adhamo0o <adhamelmesady414@gmail.com>
2024-02-11 23:15:34 -0800

    5

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""This is a class called square."""
+
+
+class Square:
+    """This class that defines a square."""
+    def __init__(self, size=0):
+        """
+        Initializes the square with the given size.
+        Args:
+            size (int): The size of a square.
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    
+    def size(self):
+        """This function returns the size."""
+        return self.size
+
+    def size(self, value):
+        """
+        Initializes the square with the given size.
+        Args:
+            size (int): The size of a square.
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Returns the current squares area
+        """
+        return self.__size ** 2
